7dd271bf24d1e9be58792651bbb1720d7422f919
Aaron <toscleveland@gmail.com>
2020-09-02 21:52:28 -0500

    Added tableview to sourcestableview

--- Newstack/Controllers/Sources/SourcesViewController.swift
+++ Newstack/Controllers/Sources/SourcesViewController.swift
@@ -8,20 +8,46 @@
 import UIKit
 
 class SourcesViewController: UIViewController {
+    
+    let sourcesTableView: UITableView = {
+        let sourcesTV = UITableView()
+        sourcesTV.translatesAutoresizingMaskIntoConstraints = false
+        return sourcesTV
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         configureNavigationController()
+        sourcesTableView.delegate = self
+        sourcesTableView.dataSource = self
     }
     
     func configureUI() {
         view.backgroundColor = .systemBackground
+        view.addSubview(sourcesTableView)
+        sourcesTableView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                leading: view.leadingAnchor,
+                                trailing: view.trailingAnchor,
+                                anchorLeading: 20,
+                                anchorTrailing: -20)
     }
     
     func configureNavigationController() {
         self.navigationItem.title = "Sources"
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+}
+
+extension SourcesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     
 }
